628127b94967a384865135a03d1d9e928852af5a
Jabi <michal.jabi@gmail.com>
2026-03-05 09:57:45 +0100

    task(01-wamup): test runtime + quokka.

--- ts-explain/src/0-Warm-up/1-in-html/script1.js
+++ ts-explain/src/0-Warm-up/1-in-html/script1.js
@@ -1,2 +1,20 @@
 let LUCKY_NUMBER = 100;
 console.log('#1. wartość LN', LUCKY_NUMBER);
+LUCKY_NUMBER;
+
+console.log( 20 + 10);
+
+
+
+function hello() {
+    console.log("!")
+}
+
+try {
+    codkod
+} catch (e) {
+    console.log(e.message);
+}
+
+console.log('!');
+
