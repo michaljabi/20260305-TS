2e16cbc5dacd2da476f773698c5012e6fe9f3e94
Jabi <michal.jabi@gmail.com>
2026-03-05 10:40:36 +0100

    explain(P01): working with primitive types + type inferance + way of TS check our code.

--- ts-explain/src/0-Warm-up/1-in-html/script1.js
+++ ts-explain/src/0-Warm-up/1-in-html/script1.js
@@ -1,20 +1,38 @@
+// // @ts-check
+// tylko w plikach .js
 let LUCKY_NUMBER = 100;
-console.log('#1. wartość LN', LUCKY_NUMBER);
-LUCKY_NUMBER;
+console.log("#1. wartość LN", LUCKY_NUMBER, 0, 0, 0, 0, 0, 0, 2, 29);
 
-console.log( 20 + 10);
+LUCKY_NUMBER = "?";
 
+console.log(20 + 10);
 
+const MY_QUESTION_MARK = "?";
+
+// MY_QUESTION_MARK = "";
 
 function hello() {
-    console.log("!")
+  throw "Hello!" // niestety tak się da w JS
+  throw new Error("Hello!") // dobry kod!
+  // console.log("!");
 }
 
 try {
-    codkod
+  hello()
+  //codkod;
 } catch (e) {
+
+    // Zapis fajowy jak migrujesz JS -> TS
+    // @ts-expect-error
     console.log(e.message);
+
+    if(e instanceof TypeError) {
+        console.log('[T] error:', e.message);
+    } else if(e instanceof Error) {
+        console.log(e.message);
+    } else {
+        console.log(e)
+    }
 }
 
-console.log('!');
-
+console.log("!");
--- ts-explain/src/P-please-explain-me-more/P01-type-inference.ts
+++ ts-explain/src/P-please-explain-me-more/P01-type-inference.ts
@@ -7,3 +7,17 @@ let y;
 y = 200;
 
 console.log(y);
+
+// union type:
+// any będzie nieprecyzyjny - to zbyt "Dużo" typów które spełniają warunek...
+// let price: any = 10;
+// Poprawny kodzik
+let price: string | number = 10;
+
+price =  '100 zł'
+
+
+// Teraz jest super bo poniższe zapisy będą błędne:
+// price = true;
+// price = {}; // new Object()
+// price = []; // new Array()
