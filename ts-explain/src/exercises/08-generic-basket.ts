491e1b4e63a4161b2fa6d5e74bce1afbeefb2830
Jabi <michal.jabi@gmail.com>
2026-03-06 15:29:31 +0100

    refactor(08): use abstract class, explain Structural Typing approach in TS.

--- ts-explain/src/exercises/08-generic-basket.ts
+++ ts-explain/src/exercises/08-generic-basket.ts
@@ -12,47 +12,63 @@
 
 // Odkomentuj w momencie rozpoczęcia pracy nad zadaniem:
 
-class BuyNowItem {
-    constructor(public name: string) {}
+abstract class BasketItem {
+  constructor(public name: string) {}
 }
 
-class AuctionItem {
-    constructor(public name: string) {}
-}
+class BuyNowItem extends BasketItem {}
 
-class Basket<ITEM> {
+class AuctionItem extends BasketItem {}
 
-    private myItems: ITEM[] = [];
+class Basket<ITEM extends BasketItem> {
+  private myItems: ITEM[] = [];
 
-    addItem(item: ITEM) {
-        this.myItems.push(item);
-    }
+  addItem(item: ITEM) {
+    this.myItems.push(item);
+  }
 
-    getAllItems(): ITEM[] {
-        return this.myItems;
-    }
+  getAllItems(): readonly ITEM[] {
+    return this.myItems;
+  }
 }
 
 const myBuyNowBasket = new Basket<BuyNowItem>();
 const myAuctionBasket = new Basket<AuctionItem>();
 
-myBuyNowBasket.addItem(new BuyNowItem('Telewizor'));
-myBuyNowBasket.addItem(new BuyNowItem('Wersalka'));
+// duck-typing.
+const myFakeBasket = new Basket<{ name: string }>();
 
-myAuctionBasket.addItem(new AuctionItem('Karta kolekcjonerska'));
+// const myStringBasket = new Basket<string>();
+
+myBuyNowBasket.addItem(new BuyNowItem("Telewizor"));
+myBuyNowBasket.addItem(new BuyNowItem("Wersalka"));
+
+myAuctionBasket.addItem(new AuctionItem("Karta kolekcjonerska"));
 
 myBuyNowBasket.getAllItems().forEach((item: BuyNowItem) => {
-    console.log(item.name);
-})
+  console.log(item.name);
+});
 
 myAuctionBasket.getAllItems().forEach((item: AuctionItem) => {
-    console.log(item.name);
-})
+  console.log(item.name);
+});
 
 // Dodatkowe punkty jeśli ta operacja będzie niemożliwa w TS:
 // myBuyNowBasket.getAllItems().push(new BuyNowItem('Troll'))
-
-
-
+// myBuyNowBasket.getAllItems().pop()
 
 export {};
+
+class Car {
+  manufacturer = "xya";
+}
+
+class Truck {
+  manufacturer = "Teslatruck..";
+  wheels = 4;
+}
+
+// Wiosek:
+const myCar: Car = new Truck();
+// TS nie jest typowany Nominalnie tylko Strukturalnie,
+// nie insteresuje mnie kim jesteś... tylko coś robisz - albo jaki masz kształt.
