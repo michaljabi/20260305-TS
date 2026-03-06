fc4291eeeaaed5ed07d773e9dccf9bfee5515c9e
Jabi <michal.jabi@gmail.com>
2026-03-06 11:55:00 +0100

    explain: type assertions (as) in TypeScript and Why they are "hacky"

--- ts-examples/6-backend-server-hono/src/cars/cars.controller.ts
+++ ts-examples/6-backend-server-hono/src/cars/cars.controller.ts
@@ -1,3 +1,5 @@
+// // @ts-nocheck
+
 import { type Context, Hono } from "hono";
 import { carsService } from "./cars.service.js";
 
@@ -10,9 +12,28 @@ carsController.get("", (c: Context) => {
 })
 
 carsController.get("/:uid", (c: Context) => {
+    // TO niżej TO NIE RZUTOWANIE!!! To tzw. asercja typu! (czyli powiedzienie "Trus Me TS, I know what I'm doing 😎")
+    // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
+    // const uid = c.req.param("uid") as string;
+    // // @ts-ignore
+    /*
+   
+    const car2 = carsService.getByUid(uid)
+    const car3 = carsService.getByUid(uid)
+    const car4 = carsService.getByUid(uid)
+    const car5 = carsService.getByUid(uid)
+    const car6 = carsService.getByUid(uid)
+    const car7 = carsService.getByUid(uid)
+    */
+    // Faktycznie rzutowanie (ale tutaj nie jest to dobry pomysł bo z undefined zrobię "undefined"):
+    // const uid = String(c.req.param("uid"));
+    // DOBRE rozwiązanie:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+    // czemu? - bo jak param uid będzie undefined to przypiszemy pusty string !
     const uid = c.req.param("uid") ?? '';
     // Zadanie #1:
     // napraw poniższy problem TS2345
+
     const car = carsService.getByUid(uid)
 
     if (!car) {
